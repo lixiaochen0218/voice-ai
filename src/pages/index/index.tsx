337edeabb811b159ffbf164730e7192fd589ce6c
Alex Li <alexli@moneybrain.ai>
2022-07-27 11:44:27 +0800

    integrate stt

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -5,13 +5,15 @@ import { useVideoUrl } from "~/module/video"
 import { useStatus } from "~/module/state"
 import Recorder from 'recorder-core'
 import 'recorder-core/src/engine/pcm'
-// import 'recorder-core/src/engine/mp3'
-// import 'recorder-core/src/engine/mp3-engine'
 import { Preset } from "./preset"
 import { Answer } from "./answer"
 import { AIVideo } from "./aiVideo"
 import { Mask, RecordBtn } from "./recordBtn"
 import "./style.css"
+
+// const STT_END_POINT = 'https://service-cy0mbn4f-1310900042.bj.apigw.tencentcs.com/release/stt';
+const STT_END_POINT = 'http://localhost:9000/stt';
+
 
 const Page: FC = () => {
   const [status, action] = useStatus()
@@ -20,22 +22,15 @@ const Page: FC = () => {
   const { URL: videoUrl, answer, appId } = useVideoUrl(message, status)
   const [recorder, setRecorder] = useState(Recorder({type:"pcm",sampleRate:16000,bitRate:16}))
 
-  const handleRecordMsg = useCallback(
-    msg => {
-      setMessage(msg)
-      if (!msg) {
-        action.done()
-      } else {
-        action.play()
-      }
-    },
-    [action]
-  )
+  useEffect(() => {
+    recorder.open()
+  }, [])
 
   const handleStart = useCallback(async () => {
     console.log("start recording")
     recorder.start()
     action.record()
+    // setTimeout(() => recorder.stop(),5000);
   }, [action])
 
   const handleStop = useCallback(async () => {
@@ -44,67 +39,41 @@ const Page: FC = () => {
     recorder.stop((blob,duration) => {
       console.log(blob,(window.URL||webkitURL).createObjectURL(blob),"时长:"+duration+"ms");
 
-      // var reader = new FileReader();
-      // reader.readAsDataURL(blob); 
-      // reader.onloadend = function() {
-      //   var base64data = reader.result.split("base64,")[1];                
-      //   console.log(base64data);
-        
-      //   const data = {
-      //     ProjectId: 0, //腾讯云项目 ID
-      //     SubServiceType: 2, //一句话识别
-      //     SourceType: 1, //语音数据
-      //     EngSerViceType: "16k_zh",
-      //     VoiceFormat: "pcm", //识别音频的音频格式。
-      //     Data: base64data,
-      //     UsrAudioKey: "chunni",
-      //     FilterDirty:1,//是否过滤脏词
-      //     FilterPunc:1,//是否过滤标点符号
-      //     FilterModal:1//是否过语气词
-      //   };
-      //   Taro.request({
-      //     url: 'asr.tencentcloudapi.com',
-      //     header: {
-      //       "X-TC-Version": "2019-06-14",
-      //       "X-TC-Region": "ap-shanghai",
-      //       "X-TC-Action": "SentenceRecognition",
-      //       "X-TC-Timestamp": 1599140479,
-      //       "Authorization": ''
-      //     },
-      //     method: 'POST',
-      //     data,
-      //     success(response) {
-      //       console.log("nlsRes", response)
-      //       // const json = JSON.parse(nlsRes.data)
-      //       // setMessage(json.result)
-      //     },
-      //     fail(e) {
-      //       console.log(e)
-      //       setMessage("")
-      //     }
-      //   })
-      // }
+      // const file = new File([blob], 'test.pcm', { type: blob.type })
+      // const filePath = (window.URL||webkitURL).createObjectURL(file)
+      // console.log(file,filePath)
 
-      // Taro.uploadFile({
-      //   url: "https://finogeeks-tools.finogeeks.club/mop-nls/upload",
-      //   filePath,
-      //   name: "file",
-      //   formData: { token: "finclip@nls", file: form },
-      //   success(nlsRes) {
-      //     console.log("nlsRes", nlsRes.data)
-      //     const json = JSON.parse(nlsRes.data)
-      //     setMessage(json.result)
-      //   },
-      //   fail(e) {
-      //     console.log(e)
-      //     setMessage("")
-      //   }
-      // })
-  },function(s){
-      console.log("handleStop -> 录音失败");
-  });
+      var reader = new FileReader();
+      reader.readAsDataURL(blob); 
+      reader.onloadend = function() {
+        var base64data = reader.result.split("base64,")[1];                
+        // console.log(base64data);
 
-    // setMessage("12321321321")
+        Taro.request({
+          url: STT_END_POINT,
+          method:"POST",
+          data:{
+            data: base64data
+          },
+          success:function(res){
+              console.log("上传成功",res);
+              if (res.data.Result === '') {
+                setMessage(" ")
+              } else {
+                setMessage(res.data.Result)
+              }
+          },
+          fail:function(res){
+              console.error("上传失败",res);
+              setMessage("")
+          }
+        });
+      }
+
+    },function(s){
+        console.log("handleStop -> 录音失败");
+    });
+
   }, [action])
 
   const toIdle = useCallback(() => {
@@ -140,19 +109,14 @@ const Page: FC = () => {
     }
   }, [toIdle, appId])
 
-  useEffect(() => {
-    // checkRecordScope()
-    recorder.open()
-  }, [])
-
   useDidHide(toIdle)
 
   return (
     <View
       className={["page","is-wechat"].join(" ")}
     >
-      <View className='video-and-text'>
-        <AIVideo src={videoUrl} onEnded={handleVideoEnd} onPlay={action.play} />
+      <View className='video-and-text' onLongPress={toIdle}>
+        <AIVideo src={videoUrl} onEnded={handleVideoEnd} onPlay={action.play}/>
         <Answer
           loading={status === "loading"}
           message={message}
